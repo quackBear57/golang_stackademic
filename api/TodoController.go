122a62d0bf5df475edf1690c8eee8faf5d418c49
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2026-04-02 18:37:28 -0500

    Update TodoController.go

---
+++ api/TodoController.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"net/http"
+	"github.com/go-chi/chi/v5"
+)
+
+type TodoResources struct {}
+
+// Routes creates a REST router for the todos resource
+func (rs TodoResources) Routes() chi.Router {
+	r := chi.NewRouter()
+	// r.Use() // some middelware..
+
+	r.Route("/{id}", func(r chi.Router) {
+		r.Get("/", rs.Get)	// GET /todos/{id} - read a single todo
+	})
+
+	return r
+}
+
+func (rs TodoResources) Get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	w.Write([]byte("todo get" + id))
+}
