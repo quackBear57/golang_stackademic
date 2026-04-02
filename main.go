a687b1c0718faf1c2c0da0804ad6cebd31a1e2a2
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2026-04-02 18:29:34 -0500

    add listening ports and path

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe(":5000", r)
 	fmt.Println("Hello World")
 }
